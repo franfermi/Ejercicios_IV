2571484a6acb972869381e7aef8280da5b972361
Francisco Fernández Millán <franfermi@correo.ugr.es>
2017-10-09 18:33:39 +0200

    Subida de proyecto

---
+++ Tema2/WebAppMVC/WebAppMVC/Controllers/EmpresaController.cs
@@ -0,0 +1,22 @@
+﻿using WebAppMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detalle(int Id)
+        {
+            Empresa model = AsociacionEmpresas.GetEmpresa(Id);
+            return View(model);
+        }
+    }
+}
---
+++ Tema2/WebAppMVC/WebAppMVC/Models/Empresa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC.Models
+{
+    public class Empresa
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Telefono { get; set; }
+    }
+
+    public class AsociacionEmpresas
+    {
+        public static Empresa GetEmpresa(int i)
+        {
+            List<Empresa> lista = new List<Empresa>()
+            {
+                new Empresa
+                {
+                    Descripcion = "Proyectos TIC globales y servicios gestionados o en la nube: handCLOUD, handTIC, formacionTIC.",
+                    Id = 1,
+                    Nombre = "ALHAMBRA-EIDOS",
+                    Telefono = "91 787 23 00"
+                },
+                new Empresa
+                {
+                    Descripcion = "Consultoría e ingeniería TIC, proyectos de comunicaciones y seguridad, CPDs, servicios gestionados y en cloud, soporte y mantenimiento.",
+                    Id = 2,
+                    Nombre = "AMBAR TELECOMUNICACIONES",
+                    Telefono = "942344896"
+                },
+                new Empresa
+                {
+                    Descripcion = "Networking, Internet de las Cosas, Comunicaciones Unificadas, Infraestructuras...",
+                    Id = 3,
+                    Nombre = "Cisco Systems",
+                    Telefono = "915907500"
+                },
+                new Empresa
+                {
+                    Descripcion = "Prestación de servicios, investigación y coordinación en materia de Ciberseguridad.",
+                    Id = 4,
+                    Nombre = "Instituto Nacional de Ciberseguridad (Incibe)",
+                    Telefono = "91 212 76 26"
+                },
+            };
+
+            return lista.FirstOrDefault(emp => emp.Id == i);
+        }
+    }
+}
